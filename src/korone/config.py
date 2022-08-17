0e2958ef3f198a90b615d17daa4fc7bf9a7792d3
Victor Cebarros <79484108+victorcebarros@users.noreply.github.com>
2022-08-17 14:53:02 -0300

    Feat: Creates configuration facilities

---
+++ src/korone/config.py
@@ -0,0 +1,66 @@
+"""Korone is a simple multipurpose Telegram Bot.
+
+This is a complete rewrite of PyKorone, refer to
+https://github.com/AmanoTeam/PyKorone and README.md for more
+information."""
+
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2022 Victor Cebarros <https://github.com/victorcebarros>
+
+
+from configparser import ConfigParser
+from os import path
+
+import os
+import sys
+
+from korone import constants
+from korone.utils import log
+
+
+config: ConfigParser = ConfigParser()
+
+config["Authentication"] = {
+    "API_ID": "",
+    "API_HASH": "",
+    "BOT_TOKEN": "",
+}
+
+
+def init(cfgpath: str = "") -> None:
+    """Initializes configuration file."""
+
+    if cfgpath == "":
+        cfgpath = constants.DEFAULT_CONFIG_PATH
+
+    dirname: str = path.dirname(cfgpath)
+
+    log.info("Initializing configuration module")
+    log.debug("Using path %s", path)
+
+    if dirname != "" and not path.isdir(dirname):
+        log.info("Could not find configuration directory, attempting to create")
+        try:
+            log.debug("Creating configuration directory")
+            os.mkdir(dirname)
+        except OSError as err:
+            log.critical("Could not create directory: %s", err)
+            sys.exit(1)
+
+    if not path.isfile(cfgpath):
+        log.info("Could not find configuration file, attempting to create")
+        try:
+            log.debug("Creating configuration file")
+            with open(cfgpath, "w", encoding="utf-8") as configfile:
+                config.write(configfile)
+        except OSError as err:
+            log.critical("Could not create configuration file: %s", err)
+
+    config.read(cfgpath)
+
+
+def get(section: str, option: str, fallback: str = "") -> str:
+    """Gets an option value for a given section.
+
+    Wraps around ConfigParser's get method."""
+    return config.get(section, option, fallback=fallback)
--- src/korone/constants.py
+++ src/korone/constants.py
@@ -8,9 +8,17 @@ information."""
 # Copyright (c) 2022 Victor Cebarros <https://github.com/victorcebarros>
 
 
+import os
+
+
 LOGGER_FORMAT_OUTPUT: str = "%(filename)s"  \
                            ":%(funcName)s"  \
                            ":%(lineno)s"    \
                            "|%(levelname)s" \
                            "|%(message)s"   \
                            "|%(created)i"
+
+XDG_CONFIG_HOME: str = os.environ.get("XDG_CONFIG_HOME", "~/.config")
+XDG_DATA_HOME: str = os.environ.get("XDG_DATA_HOME", "~/.local/share")
+
+DEFAULT_CONFIG_PATH: str = f"{XDG_CONFIG_HOME}/korone/korone.conf"
