b63f196049e4384dd1861df7372a603b99c6df9e
Victor Cebarros <79484108+victorcebarros@users.noreply.github.com>
2022-08-27 18:28:26 -0300

    Refactor: Rewrites hello module

--- src/korone/commands/hello.py
+++ src/korone/commands/hello.py
@@ -17,20 +17,42 @@ from korone.utils import log
 from korone.locale import StringResource
 
 
-@Client.on_message(filters=filters.command("hello"))  # type: ignore
-async def command_hello(client: Client, message: Message) -> None:
+def get_language_code(message: Message) -> str:
+    """Gets language code from message."""
+    language_code: str = "en"
+    if message.from_user is None:
+        return language_code
+
+    if message.from_user.language_code is None:
+        return language_code
+
+    language_code = message.from_user.language_code
+
+    return language_code
+
+
+@Client.on_message(group=1)
+async def catchall(_a: Client, _b: Message) -> None:
+    """Catches all messages from user."""
+
+    log.debug("New message!")
+
+
+@Client.on_message(filters.command("greet"))
+async def command_greet(_: Client, message: Message) -> None:
     """Says hello."""
-    log.info("user started interaction")
+    language_code: str = get_language_code(message)
 
-    if None in (client, message, message.chat, message.from_user):
-        return
+    await message.reply(
+        StringResource.get(language_code, "strings/greet/message")
+    )
 
-    if None in (message.chat.id, message.from_user.language_code):
-        return
 
-    language_code: str = message.from_user.language_code
+@Client.on_message(filters.command("farewell"))
+async def command_farewell(_: Client, message: Message) -> None:
+    """Says farewell."""
+    language_code: str = get_language_code(message)
 
-    await client.send_message(
-            chat_id=message.chat.id,
-            text=StringResource.get(language_code, "strings/hello/message")
+    await message.reply(
+        StringResource.get(language_code, "strings/farewell/message")
     )
