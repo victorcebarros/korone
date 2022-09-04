796a44d712a012620db821b58c707dfa83b50303
Hitalo <hitalo331@outlook.com>
2022-09-04 13:30:47 -0300

    Docs: Updates tests and commands manager doc

---
+++ tests/__init__.py
@@ -0,0 +1,6 @@
+"""
+Tests Korone's implementations.
+"""
+
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2022 Victor Cebarros <https://github.com/victorcebarros>
--- tests/test_user_manager.py
+++ tests/test_user_manager.py
@@ -1,7 +1,9 @@
-"""Korone is a simple multipurpose Telegram Bot.
-This is a complete rewrite of PyKorone, refer to
-https://github.com/AmanoTeam/PyKorone and README.md for more
-information."""
+"""
+Tests for the user manager.
+"""
+
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2022 hytalo-bassi <https://github.com/hytalo-bassi>
 
 from faker import Faker
 from pyrogram.types import User
