4d6a5d43355ee560fc77188d5cb5cb7bbeb9d20b
Victor Cebarros <79484108+victorcebarros@users.noreply.github.com>
2022-09-08 03:20:30 -0300

    Feat: Implements BFS attribute search algorithm

--- src/korone/utils/traverse.py
+++ src/korone/utils/traverse.py
@@ -63,3 +63,61 @@ def traverse(tree: Union[dict, list], path: str, separator: str = "/") -> Any:
         raise KeyError(f"Trying to access nonexistent nodes: {node}")
 
     return tree
+
+
+def bfs_attr_search(root: object, attr: str) -> object:
+    """
+    Searches for attribute in object using the BFS algorithm.
+
+    Note: attributes starting with '_' are ignored.
+
+        .. code-block:: python
+            >>> def fun():
+            ...     pass
+            >>> fun.hello = lambda x: x * 2
+            >>> fun.hello.again = lambda x: x / 4
+            >>> fun.hallo = lambda x: x - 7
+            >>> fun.hallo.wieder = lambda x: x ** 3
+            >>> fun.hola = lambda x: x + 2
+            >>> fun.hola.otravez = lambda x: x * 8
+            >>> bfs_attr_search(fun, "again")
+            <function <lambda> at ???>
+            >>> bfs_attr_search(fun, "again")(20)
+            5.0
+
+    :param root: Root of search
+    :type root:  object
+    :param attr: Attribute name to search
+    :type attr:  str
+
+    :return:     The attribute with name attr found in search
+    :rtype:      object
+
+    :raises AttributeError: When could not find the attribute
+    """
+
+    queue: list = []
+    visited: list = []
+
+    queue.append(root)
+    visited.append(id(root))
+
+    while queue:
+        obj = queue.pop()
+
+        if hasattr(obj, attr):
+            return getattr(obj, attr)
+
+        try:
+            objs = map(lambda attr: getattr(obj, attr),
+                       filter(lambda s: not s.startswith("_"),
+                              vars(obj)))
+        except TypeError as err:
+            raise AttributeError(f"Could not find attribute {attr}") from err
+
+        for neighbor in objs:
+            if id(neighbor) not in visited:
+                queue.append(neighbor)
+                visited.append(id(neighbor))
+
+    raise AttributeError(f"Could not find attribute {attr}")
