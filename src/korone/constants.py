ae10329774768690e31e04dced999ffe08accc4b
Hitalo <hitalo331@outlook.com>
2022-09-07 23:31:43 -0300

    Docs: Improves constants documentation

--- src/korone/constants.py
+++ src/korone/constants.py
@@ -1,35 +1,6 @@
 """
 All Korone's constants that are used throughout the codebase.
 
-.. data:: LOGGER_FORMAT_OUTPUT
-    The format to be used in the logger.
-
-.. data:: DATABASE_SETUP
-    The database setup to be used.
-
-.. data:: MODULES_PACKAGE_NAME
-    The package that contains all the commands modules.
-
-.. data:: DEFAULT_NAME
-    The default bot name to be used when no name is provided.
-
-.. data:: DEFAULT_WORKERS
-    The default number of workers to be used when no number is provided.
-
-.. data:: DEFAULT_CONFIG_PATH
-    The default path to the config file.
-
-.. data:: DEFAULT_DBFILE_PATH
-    The default path to the database file.
-
-.. data:: XDG_CONFIG_HOME
-    The XDG_CONFIG_HOME environment variable.
-    Where user-specific configurations should be written (analogous to /etc).
-
-.. data:: XDG_DATA_HOME
-    The XDG_DATA_HOME environment variable.
-    Where user-specific data files should be written (analogous to /usr/share).
-
 .. note::
     For more information related to the XDG constants, see: `XDG Base Directory`_.
 
@@ -50,15 +21,28 @@ LOGGER_FORMAT_OUTPUT: str = (
     "|%(message)s"
     "|%(created)i"
 )
+"The format to be used in the logger."
 
 XDG_CONFIG_HOME: str = os.environ.get("XDG_CONFIG_HOME", "~/.config")
+"""
+The XDG_CONFIG_HOME environment variable.
+Where user-specific configurations should be written (analogous to /etc).
+"""
 XDG_DATA_HOME: str = os.environ.get("XDG_DATA_HOME", "~/.local/share")
+"""
+The XDG_DATA_HOME environment variable.
+Where user-specific data files should be written (analogous to /usr/share).
+"""
 
 DEFAULT_CONFIG_PATH: str = f"{XDG_CONFIG_HOME}/korone/korone.conf"
+"The default path to the config file."
 DEFAULT_DBFILE_PATH: str = f"{XDG_DATA_HOME}/korone/korone.db"
+"The default path to the database file."
 
 DEFAULT_WORKERS: int = 24
+"The default number of workers to be used when no number is provided."
 DEFAULT_NAME: str = "korone"
+"The default Pyrogram client name to be used when no name is provided."
 
 DATABASE_SETUP: str = """\
 CREATE TABLE IF NOT EXISTS Users (
@@ -92,5 +76,7 @@ VACUUM;
 
 PRAGMA journal_mode="WAL";
 """
+"The database setup to be used."
 
 MODULES_PACKAGE_NAME: str = "korone.commands"
+"The package that contains all the commands modules."
